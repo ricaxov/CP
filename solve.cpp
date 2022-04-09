828a43c69fde67054505579dbe3c0608957b3fb8
ricaxov <103327245+ricaxov@users.noreply.github.com>
2022-04-09 13:23:38 -0300

    Create solve.cpp

---
+++ solve.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define pi 3.14159
+#define pb push_back
+#define ll long long
+#define ii pair<int,int>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+}
