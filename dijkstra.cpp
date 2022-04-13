5fdf94d9ef2a7a0a041049bd33979f36bb541d21
ricaxov <103327245+ricaxov@users.noreply.github.com>
2022-04-13 15:19:55 -0300

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -14,25 +14,25 @@ using namespace std;
 
 vii adj[maxN];
 int dijkstra(int o, int d){
-	vi dist(maxN, inf);
-	priority_queue<ii, vii, greater<ii> >pq;
-	vi pai(maxN, -1);
-	dist[o]=0;
-	pq.push({0,o});
-	while(!pq.empty()){
-		ii u=pq.top(); pq.pop();
-		if(dist[u.s]!=u.f) continue;
-		for(auto v : adj[u.s]){
-			int to=v.s;
-			int len=v.f;
-			if(dist[to]>len+dist[u.s]){
-				dist[to]=len+dist[u.s];
-				pai[to]=u.s;
-				pq.push({dist[to],to});
-			}
-		}
-	}
-	return dist[d];
+    vi dist(maxN, inf);
+    priority_queue<ii, vii, greater<ii> >pq;
+    vi pai(maxN, -1);
+    dist[o]=0;
+    pq.push({0,o});
+    while(!pq.empty()){
+        ii u=pq.top(); pq.pop();
+        if(dist[u.s]!=u.f) continue;
+        for(auto v : adj[u.s]){
+            int to=v.s;
+            int len=v.f;
+            if(dist[to]>len+dist[u.s]){
+                dist[to]=len+dist[u.s];
+                pai[to]=u.s;
+                pq.push({dist[to],to});
+            }
+        }
+    }
+    return dist[d];
 }
 int main(){
     ios_base::sync_with_stdio(false);cin.tie(NULL);
