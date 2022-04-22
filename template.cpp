d00bd37b7afaec9d6aecf1749f2c7eb5fcb76c36
ricaxov <103327245+ricaxov@users.noreply.github.com>
2022-04-22 18:03:30 -0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,13 +1,19 @@
 #include <bits/stdc++.h>
-#define inf 1e9
+
+using namespace std;
+
 #define f first
 #define s second
 #define pi 3.14159
 #define pb push_back
-#define ll long long
-#define ii pair<int,int>
+#define tos to_string
+#define mod 1000000007
+#define inf 1000000000000000000
+#define entrada freopen("input.txt", "r", stdout);
+#define saida freopen("output.txt". "w". stdin);
+typedef long long ll;
+typedef pair<int,int> ii;
 
-using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(false);cin.tie(NULL);
