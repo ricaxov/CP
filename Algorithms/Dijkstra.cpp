c68dc7173f4c5fefc6f2be01f63d828c6676f41b
ricaxov <103327245+ricaxov@users.noreply.github.com>
2022-05-27 16:59:51 -0300

    Update Dijkstra.cpp

--- Algorithms/Dijkstra.cpp
+++ Algorithms/Dijkstra.cpp
@@ -7,12 +7,9 @@ int dijkstra(int o, int d){
     pq.push({0,o});
     while(!pq.empty()){
         ii u=pq.top(); pq.pop();
-        if(dist[u.s]!=u.f){
-            continue;
-        }
         for(auto v : adj[u.s]){
-            int to=v.s;
-            int len=v.f;
+            int to=v.f;
+            int len=v.s;
             if(dist[to]>len+dist[u.s]){
                 dist[to]=len+dist[u.s];
                 pai[to]=u.s;
